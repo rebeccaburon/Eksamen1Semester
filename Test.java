ab066bba82226e15b25b43dadbc6c8f8ba926d5e
rebeccaburon <rebecca.mary.buron@hotmail.com>
2023-12-20 13:17:23 +0100

    test

---
+++ Test.java
@@ -0,0 +1,22 @@
+public class Test {
+    private String name;
+    private int age;
+    private int donations;
+
+    public Test(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.donations=donations;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+}
+
+
